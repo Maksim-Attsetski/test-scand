49185870d39ffaed88482def608f2ae0d73a32ed
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-04-10 18:09:02 +0300

    add images and setup ts config

---
+++ src/assets/index.ts
@@ -0,0 +1,26 @@
+import bg1 from './test-data/bg/img-1.png';
+import bg2 from './test-data/bg/img-2.png';
+import bg3 from './test-data/bg/img-3.png';
+import bg4 from './test-data/bg/img-4.png';
+import bg5 from './test-data/bg/img-5.png';
+import bg6 from './test-data/bg/img-6.png';
+
+import avatar1 from './test-data/avatars/avatar-1.png';
+import avatar2 from './test-data/avatars/avatar-2.png';
+import avatar3 from './test-data/avatars/avatar-3.png';
+
+import logo from './logo.png';
+
+export const testImages = {
+  bg1,
+  bg2,
+  bg3,
+  bg4,
+  bg5,
+  bg6,
+  avatar1,
+  avatar2,
+  avatar3,
+};
+
+export const imgs = { logo };
